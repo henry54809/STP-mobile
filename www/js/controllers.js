8f7632a51d6d01867918e591c806dbc4a9720713
slin77 <slin77@gatech.edu>
2015-01-18 17:37:03 -0500

    sign up UI controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,37 +8,59 @@ angular.module('starter.controllers', [])
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    id: 1,
+    $scope.signin_modal = modal;
   });
+
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
+    scope: $scope
+  }).then(function(modal) {
+    id: 2,
+    $scope.signup_modal = modal;
+  });
+
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.signin_modal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.signin_modal.show();
   };
+
+  $scope.signup = function() {
+    $scope.signin_modal.hide()
+    console.log("redirect to signup");
+    $scope.signup_modal.show();
+
+  }
+
+  $scope.closeSignup = function() {
+     console.log("calling close Signup")
+    $scope.signup_modal.hide();
+  }
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
+
     console.log('Doing login', $scope.loginData);
 
-    //console.log(1234);
-        var userName = $scope.loginData.username;
-        console.log(userName);
-        var pw = $scope.loginData.password;
-        if (typeof userName == 'undefined') {
-            alert("Username is empty");
-            return
-        }
-        if (typeof pw == 'undefined') {
-            alert("Password is empty");
-            return
-        }
+    // //console.log(1234);
+    //     var userName = $scope.loginData.username;
+    //     console.log(userName);
+    //     var pw = $scope.loginData.password;
+    //     if (typeof userName == 'undefined') {
+    //         alert("Username is empty");
+    //         return
+    //     }
+    //     if (typeof pw == 'undefined') {
+    //         alert("Password is empty");
+    //         return
+    //     }
 
-        accountService.logIn(JSON.stringify($scope.loginData));
+    //     accountService.logIn(JSON.stringify($scope.loginData));
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
