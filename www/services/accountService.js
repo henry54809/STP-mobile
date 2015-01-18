cb81fe1ed55ac89e61911855da2a016613638a52
davidzhu918 <zzhu72@gatech.edu>
2015-01-18 16:01:47 -0500

    sss

--- www/services/accountService.js
+++ www/services/accountService.js
@@ -33,30 +33,31 @@ starter.service('accountService',['$location','$http','$window', function ($loca
   var that = this;
   this.logIn = function(user){
     console.log("accountService called")
-     $http.post('./php/login.php', user,null)
-      .success(function (data, status, headers, config)
-        { 
-          // console.log(data);
-          if (typeof data['isvalid'] == 'undefined') {
-            $window.alert('验证失败')
-            // errorCode = 1;
-          } else {
-            if (data['isvalid'] == true) {
-                // console.log(data['entity_pk']['entity']);
-                // console.log($cookies.uid);
-                that.notifyLoggedInObservers(true);
-                $location.path("/home/"+data['entity_pk']['entity']); //should route to user's homepage. code here used for test
-            } else {
-                $window.alert('用户名或密码错误');
-                // errorCode = 2;
-            }
-          }
-        })
-        .error(function (data, status, headers, config)
-        {
-                $window.alert('Something went wrong...');
-                //errorCode = 3;
-        });
+     // $http.post('./php/login.php', user,null)
+     //  .success(function (data, status, headers, config)
+     //    { 
+     //      // console.log(data);
+     //      if (typeof data['isvalid'] == 'undefined') {
+     //        $window.alert('验证失败')
+     //        // errorCode = 1;
+     //      } else {
+     //        if (data['isvalid'] == true) {
+     //            // console.log(data['entity_pk']['entity']);
+     //            // console.log($cookies.uid);
+     //            that.notifyLoggedInObservers(true);
+     //            $location.path("/home/"+data['entity_pk']['entity']); //should route to user's homepage. code here used for test
+     //        } else {
+     //            $window.alert('用户名或密码错误');
+     //            // errorCode = 2;
+     //        }
+     //      }
+     //    })
+     //    .error(function (data, status, headers, config)
+     //    {
+     //            $window.alert('Something went wrong...');
+     //            //errorCode = 3;
+     //    });
+     
   }
 
 }]);
