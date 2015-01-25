0aca6c1f8b4a0a65c9dfa77e56aa518b923e7144
Haoli Du <henry54809@gmail.com>
2015-01-25 15:31:03 -0500

    Added friend request related API calls

--- server/app.js
+++ server/app.js
@@ -21,42 +21,45 @@ global.OK = "OK";
 global.ERROR = "Error";
 
 //COR settings
-var res_headers = function(req, res, next) {
+var res_headers = function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true' );
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 };
 
 var whitelist = ['http://localhost:8100', 'http://m.picwo.com'];
 var cors_options = {
-      origin: function(origin, callback){
-              var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-                  callback(null, originIsWhitelisted);
-     }
+  origin: function (origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  }
 };
 
-app.use( res_headers );
-app.all('/api/*', cors( cors_options ) );
+app.use(res_headers);
+app.all('/api/*', cors(cors_options));
 app.set('port', 3000);
 
 require("./auth/index")(app);
 
 var auth_functions = require('./auth/functions');
-app.all('/api/*', auth_functions.require_authentication );
+app.all('/api/*', auth_functions.require_authentication);
 
 //Routes
 require("./account/index")(app);
 require("./user/index")(app);
+require("./user/friend_actions")(app);
 
 //Create the server
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + server.address().port);
 });
 
-app.use(function(req, res ){
-	var resp = {};
-	resp.status = ERROR;
-	resp.message = "Not Supported.";
-    res.status(404).json({"message":"Not supported."});
+app.use(function (req, res) {
+  var resp = {};
+  resp.status = ERROR;
+  resp.message = "Not Supported.";
+  res.status(404).json({
+    "message": "Not supported."
+  });
 });
---
+++ server/user/friend_actions.js
@@ -0,0 +1,95 @@
+var pg = require('pg');
+
+module.exports = function (app) {
+    var express = require('express');
+    var router = express.Router();
+    var FRIEND_REQUEST_SENT = 'Friend request sent';
+    var FRIEND = 'Friend';
+    var STARRED = 'Starred';
+    var UNFRIENDED = 'Unfriended';
+
+    router.post('/:entity', function (req, res, next) {
+        var action = req.query.action;
+        if (action !== 'add') {
+            next();
+        }
+
+        var resp = {};
+        var entity = req.entity;
+        var my_entity = req.my_entity;
+        var callback = function (friend_type) {
+            if (friend_type) {
+                switch (friend_type) {
+                case FRIEND_REQUEST_SENT:
+                    resp.status = ERROR;
+                    resp.message = "Friend request exists.";
+                    res.json(resp);
+                    break;
+                case FRIEND, STARRED:
+                    resp.status = ERROR;
+                    resp.message = "Already friend.";
+                    res.json(resp);
+                    break;
+                }
+            }
+
+            //Add friend
+            var query = 'select fn_new_entity_friend (                          \
+                                                        $1,                     \
+                                                        $2,                     \
+                                                        (                           \
+                                                            select friend_type      \
+                                                              from tb_friend_type   \
+                                                             where label = $3       \
+                                                        )                           \
+                                                      )';
+            pg.connect(connectionString, function (err, client, done) {
+                client.query(query, [my_entity, entity, FRIEND_REQUEST_SENT], function (err, result) {
+                    done();
+                    if (result && result.rows[0]) {
+                        resp.status = OK;
+                        resp.message = "Friend request sent.";
+                        return res.json(resp);
+                    } else {
+                        console.log(err);
+                        resp.status = ERROR;
+                        resp.message = "Error when sending friend request.";
+                        return res.json(resp);
+                    }
+                });
+            });
+        };
+        var user_functions = require('./functions');
+        return user_functions.if_entity_friend(my_entity, entity, callback);
+    });
+
+    router.post('/:entity', function (req, res, next) {
+        var action = req.query.action;
+        if (action !== 'decline') {
+            next();
+        }
+        var resp = {};
+
+        return res.end();
+    });
+
+    router.post('/:entity', function (req, res, next) {
+        var action = req.query.action;
+        var resp = {};
+        if (action !== 'delete') {
+            next();
+        }
+
+        return res.end();
+    });
+
+    //fall through
+    router.post('/:entity', function (req, res, next) {
+        var resp = {};
+        resp.status = ERROR;
+        resp.message = "User action does not exist.";
+        res.json(resp);
+    });
+
+    app.use('/api/user', router);
+};
