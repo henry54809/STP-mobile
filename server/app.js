877ea39703c1548d43373cfddd5c95ce1ee5dcee
Haoli Du <henry54809@gmail.com>
2015-01-24 23:47:53 -0500

    Configured headers to allow multiple origins from a whitelist

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var pg = require('pg');
 var app = express();
- 
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,15 +20,24 @@ global.connectionString = "pg://webuser:8rucShn3t3pew4db@10.0.1.126/stp?ssl=true
 global.OK = "OK";
 global.ERROR = "Error";
 
+//COR settings
 var res_headers = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8100' );
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept');
   res.header('Access-Control-Allow-Credentials', 'true' );
   next();
 };
 
+var whitelist = ['http://localhost:8100', 'http://m.picwo.com'];
+var cors_options = {
+      origin: function(origin, callback){
+              var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+                  callback(null, originIsWhitelisted);
+     }
+};
+
 app.use( res_headers );
+app.all('/api/*', cors( cors_options ) );
 app.set('port', 3000);
 
 require("./auth/index")(app);
