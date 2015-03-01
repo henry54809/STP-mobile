9be9a1674b7227b2f5b95d1a3d4fcf50cf521c4d
Haoli Du <henry54809@gmail.com>
2015-03-01 17:05:57 -0500

    API request s3 upload url

--- server/upload/index.js
+++ server/upload/index.js
@@ -4,10 +4,60 @@ module.exports = function (app) {
 	var express = require('express');
 	var router = express.Router();
 
-	router.post('/', function (req, res) {
-		console.log(req.body); // form fields
-		console.log(req.files); // form files
-		res.status(204).end();
+	router.all('/', function (req, res, next) {
+		var resp = {};
+		var msg = req.body;
+		var files = msg.files;
+		if (!files) {
+			resp.status = ERROR;
+			resp.message = "Files metadata is required.";
+			res.status(400).json(resp);
+		}
+		for (var i = 0; i < files.length; i++) {
+			var file = files[i];
+			if (!file.name || !file.type || file.size) {
+				resp.status = ERROR;
+				resp.message = "Files metadata missing required attributes.";
+				res.status(400).json(resp);
+			}
+		}
+		var insert_temp_table_callback = function () {
+			var query = 'insert into tt_file( name, type, size ) values ';
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				query = query + '( ' + file.name + ', ' + file.type + ', ' + file.size + ' ),';
+			}
+			query = query.replace(/,$/g, '');
+			client.query(query, function (err, result) {
+				done();
+				if (result) {
+					return next();
+				} else {
+					resp.status = ERROR;
+					resp.message = "Error when uploading files.";
+					if (err) {
+						console.log(err);
+					}
+					return res.status(500).json(resp);
+				}
+			});
+		};
+		pg.connect(connectionString, function (err, client, done) {
+			var query = 'create temp table tt_file( name, type varchar(64), size integer)';
+			client.query(query, function (err, result) {
+				done();
+				if (result) {
+					return insert_temp_table_callback();
+				} else {
+					resp.status = ERROR;
+					resp.message = "Error when uploading files.";
+					if (err) {
+						console.log(err);
+					}
+					return res.status(500).json(resp);
+				}
+			});
+		});
 	});
-	app.use('/api/uploads', router);
+	app.use('/api/upload', router);
 };
---
+++ server/upload/trip.js
@@ -0,0 +1,75 @@
+var pg = require('pg');
+var aws = require('../util/aws');
+
+module.exports = function (app) {
+	var express = require('express');
+	var router = express.Router();
+
+	router.post('/trip', function (req, res) {
+		var resp = {};
+		var msg = req.body;
+		var cookies = req.cookies;
+		if (!msg || !msg.trip || !msg.files) {
+			resp.status = ERROR;
+			resp.message = "Missing trip id or files to upload.";
+			return res.json(resp);
+		}
+		var trip_name = msg.name;
+		if (!trip_name) {
+			trip_name = "Untitled";
+		}
+
+		var urls = [];
+
+		var get_url_callback = function (name, url) {
+			urls.push({
+				name: url
+			});
+			console.log(urls);
+			if (urls.length === msg.files.length) {
+				resp.status = OK;
+				resp.urls = urls;
+				return res.json(resp);
+			}
+		};
+
+		pg.connect(connectionString, function (err, client, done) {
+			var query = "insert into tb_file (									\
+													file_type,					\
+													path,						\
+													name,						\
+													creator						\
+												  ) 							\
+		 					                      (								\
+		 					                      	select fn_get_or_create_file_type(tt.type),			\
+		 					                      		   regexp_replace( random()::text, '^0.', '' ) 	\
+		 					                      		   || fn_random_text_md5(20)					\
+		 					                      		   || regexp_replace( tt.name, '\S*\.', '.' ), 	\
+		 					                      		   tt.name,				\
+		 					                      	  	   s.entity				\
+		 					                      	  from tb_session s,		\
+			tt_file tt\
+		 					                      	 where session_id_hash = $1 \
+		 					                      )\
+							 returning path, name";
+			client.query(query, [cookies['AuthToken']], function (err, result) {
+				done();
+				if (result && result.rows[0]) {
+					for (var i = 0; i < result.rows.length; i++) {
+						var row = result.rows[i];
+						aws.get_upload_url('stp.image', row.name, row.path, get_url_callback);
+					}
+				} else {
+					resp.status = ERROR;
+					resp.message = "Error when uploading files.";
+					if (err) {
+						console.log(err);
+					}
+					return res.status(500).json(resp);
+				}
+			});
+		});
+
+	});
+	app.use('/api/upload', router);
+};
