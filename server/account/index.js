ce35e8f4f27c7581e652086780f2557bb1a91281
Haoli Du <henry54809@gmail.com>
2015-03-08 16:43:26 -0400

    API exclude urls for auth

--- server/account/index.js
+++ server/account/index.js
@@ -139,10 +139,10 @@ module.exports = function (app) {
     var resp = {};
 
     //next if url param contains keys.
-    if (Object.getOwnPropertyNames(req.query).length > 0) {
+    if (req.query && req.query.reset_password > 0) {
       return next();
-    }    
-        
+    }
+
     var callback = function (session_valid) {
       if (!session_valid) {
         resp.status = ERROR;
@@ -258,7 +258,7 @@ module.exports = function (app) {
 
   router.put('/', function (req, res, next) {
     var query = req.query;
-    if (req.body && ( !req.values || req.values.length === 0 ) ) {
+    if (req.body && (!req.values || req.values.length === 0)) {
       return next();
     }
     var resp = {};
